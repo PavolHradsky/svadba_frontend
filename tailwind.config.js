286ae7586bf3d6ee67f9b03c2ae24042b15a715a
Pavol Hradský <71588191+PavolHradsky@users.noreply.github.com>
2023-02-18 14:20:45 +0100

    done homepage for pc

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     extend: {
       dropShadow: {
         'custom': '-10px 0px 20px rgba(0, 0, 0, 0.15)'
+      },
+      fontFamily: {
+        'cinzel': ['Cinzel', 'serif'],
+        'msmadi': ['Ms Madi', 'cursive'],
       }
     },
   },
